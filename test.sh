0e05fcda555288cc15ba9bbc4c4a8a4e1a6cf9fd
Ajith <ajithar204@gmail.com>
2023-12-01 14:10:08 +0530

    add test dockerfile and bash script for testing

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# folder names for the images
+IMAGES=("identity-service")
+
+for IMAGE in $IMAGES;
+do
+    # Set the image name
+    IMAGE_NAME="$IMAGE-test"
+
+    # Build the Docker image
+    docker build -t $IMAGE_NAME -f ./$IMAGE/Dockerfile.test ./$IMAGE
+
+    # Run the Docker container
+    docker run -it --rm --env-file=./.env $IMAGE_NAME sh -c "go test -v ./..."
+done
